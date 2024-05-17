1520a6229ea0fd00314f74d8429e2dcceadadeea
jinaryou <jryou@go.pasadena.edu>
2024-05-16 20:05:58 -0700

    begin

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let fontL, fontR, fontM, fontB, fontBk;
+
+function preload() {
+  fontL = loadFont("fonts/Roboto-Light.ttf");
+  fontR = loadFont("fonts/Roboto-Regular.ttf");
+  fontM = loadFont("fonts/Roboto-Medium.ttf");
+  fontB = loadFont("fonts/Roboto-Bold.ttf");
+  fontBk = loadFont("fonts/Roboto-Black.ttf")
+}
+
+function setup() {
+  createCanvas(800, 2500);
+}
+
+function draw() {
+  background(220);
+}
